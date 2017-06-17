17ef879a5accd7331c547cbb44069f7cc9e68332
Elliot Alderton <ellalderton@gmail.com>
2017-06-17 12:35:32 +0100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Misery', pages: 101 },
+		{ title: 'The Shining', pages: 204 },
+		{ title: 'IT', pages: 320 },
+		{ title: 'The Stand', pages: 412 }
+	]
+}
